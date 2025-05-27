1b22e04701e5abc0c985cc41d5a723e547254d68
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2025-05-27 20:49:38 +0530

    Multithreading

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // this will give the name of the current thread that is running
+        System.out.println(Thread.currentThread().getName());
+    }
+}
